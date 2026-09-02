28f701dd58e0d398df8aa4b4d3bf4a416370cefe
jotavgalves <jvgacontato@gmail.com>
2026-09-02 14:48:53 -0300

    fix: assemble complete OG image payload

--- functions/og-image.jpg.js
+++ functions/og-image.jpg.js
@@ -2,11 +2,12 @@ import p0 from '../src/og/part0.js';
 import p1 from '../src/og/part1.js';
 import p2 from '../src/og/part2.js';
 import p3 from '../src/og/part3.js';
+import p3b from '../src/og/part3b.js';
 import p4 from '../src/og/part4.js';
 import p5 from '../src/og/part5.js';
 import p6 from '../src/og/part6.js';
 
-const DATA = p0 + p1 + p2 + p3 + p4 + p5 + p6;
+const DATA = p0 + p1 + p2 + p3 + p3b + p4 + p5 + p6;
 
 function decodeBase64(value) {
   const binary = atob(value);
